7cb99f62fc51d3695583669229cdedbe6013b510
f5song <folkkykytak@gmail.com>
2025-03-18 01:41:32 +0700

    finish

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,8 +1,6 @@
-import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import { fetchCardData } from "@/app/lib/data";
 import { Suspense } from "react";
 import CardWrapper from "@/app/ui/dashboard/cards";
 import {
@@ -10,6 +8,11 @@ import {
   LatestInvoicesSkeleton,
   CardsSkeleton
 } from "@/app/ui/skeletons";
+
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+    title: 'Dashboard',
+  };
 
 export default async function Page() {
   return (
